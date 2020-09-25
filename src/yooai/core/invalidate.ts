94e840e55390b75ee50edad65e6e303022f25298
Ilya Malanin <loremipsum.inside@gmail.com>
2020-09-25 18:40:15 +0300

    fix: Invalidation decorator became more strict and now always require existing of getter and setter. Also @invalidate now can be applied to methods, invalidation will be triggered after method completion.

--- src/yooai/core/invalidate.ts
+++ src/yooai/core/invalidate.ts
@@ -1,18 +1,34 @@
-// tslint:disable
+import {isAbstractComponent} from "../layout/utils";
+import {AbstractComponent} from "./AbstractComponent";
 import {InvalidationType} from "./InvalidationType";
 
 export function invalidate(invalidationType: InvalidationType) {
-  return function <T>(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<T>) {
-    const set = descriptor.set;
+  return function <T>(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const {set, get} = descriptor;
     if (set !== undefined) {
-      descriptor.set = function(value: T) {
-        // @ts-ignore
-        if (this[propertyKey] === value) {
+      if (get === undefined) {
+        throw new Error(`To get proper invalidation the getter for "${propertyKey}" must be defined`);
+      }
+      descriptor.set = function(this:AbstractComponent, value: T) {
+        if (get.call(this) === value) {
           return;
         }
         set.call(this, value);
-        // @ts-ignore
         this.invalidate(invalidationType);
+      };
+    } else if (get !== undefined) {
+      throw new Error(
+        `You can invalidate property ${propertyKey}, it has only getter. Invalidation decorator must be applied to setter`,
+      );
+    } else if (typeof descriptor.value === "function") {
+      const originalMethod = descriptor.value;
+      descriptor.value = function(this: AbstractComponent, ...args: any[]): T {
+        if (!isAbstractComponent(this)) {
+          throw new Error("Object is not a component, it's can't be invalidated");
+        }
+        const result = originalMethod.call(this, ...args);
+        this.invalidate(invalidationType);
+        return result;
       };
     }
   };
