b7e5a4f54c4061c59aef3ef0b0f23edfca0a5435
Ilya Malanin <loremipsum.inside@gmail.com>
2022-01-13 16:18:22 +0100

    fix: Fixed `addChild` signature compatibility for AbstractBox

--- src/yooai/containers/AbstractBox.ts
+++ src/yooai/containers/AbstractBox.ts
@@ -96,13 +96,14 @@ export abstract class AbstractBox extends Pane {
     super(parent, x, y);
   }
 
-  public addChild<T extends DisplayObject>(firstChild: T, ...children: DisplayObject[]): T {
-    [firstChild, ...children].forEach((child) => {
+  public addChild<T extends DisplayObject[]>(...children: T): T[0] {
+    children.forEach((child) => {
       if (isComponent(child)) {
         child.on('resize', this.invalidateSize, this);
       }
     });
-    return super.addChild(firstChild, ...children);
+    const [firstChild, ...rest] = children;
+    return super.addChild(firstChild, ...rest);
   }
 
   public addChildAt<T extends DisplayObject>(child: T, index: number): T {
