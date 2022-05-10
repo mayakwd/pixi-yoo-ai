26b242d7763025af4b2bca4a18552c8879bf168d
Ilya Malanin <loremipsum.inside@gmail.com>
2022-05-10 22:35:35 +0200

    feat: Added loader middleware supports

--- src/yooai/theme/Theme.ts
+++ src/yooai/theme/Theme.ts
@@ -1,4 +1,7 @@
+import { Texture } from '@pixi/core';
+import { ILoaderMiddleware } from '@pixi/loaders';
 import { TextStyle } from '@pixi/text';
+import { DisplayObjectWithSize } from '../display/DisplayObjectWithSize';
 
 export class Theme {
   public defaultTextStyle: TextStyle = new TextStyle({
@@ -6,6 +9,12 @@ export class Theme {
     fontSize: '14',
     fill: 0xffffff
   });
+
+  public loaderPreMiddleware?: ILoaderMiddleware = undefined;
+  public loaderMiddleware?: ILoaderMiddleware = undefined;
+  public cacheProvider?: (url: string) => Texture | undefined = undefined;
+  public defaultPreloaderFactory?: () => DisplayObjectWithSize;
+  public defaultPreloaderUpdater?: (preloader: DisplayObjectWithSize, progress: number) => void;
 }
 
 export const theme = new Theme();
